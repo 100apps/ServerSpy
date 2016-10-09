03e58ec550c1e22fa072670ee196220755024ac4
马云腾 <gf@gfzj.us>
2016-10-09 11:45:42 +0800

    修改二维码生成算法
    通过[qrcodejs](https://github.com/davidshimjs/qrcodejs)生成二维码，这样就不用在线请求了，速度明显提升。

--- popup/server_detail.js
+++ popup/server_detail.js
@@ -3,11 +3,17 @@ function $(id){return document.getElementById(id);}
 function html(id,html){$(id).innerHTML=html};
 
 html("header",headers.header);
-$("qrcode").src="http://qr.liantu.com/api.php?text="+encodeURIComponent(headers.url);
-if($("qrcode").src.length>100){
-	$("qrcode").width=$("qrcode").height=$("qrcode").src.length;
-}else{
-	$("qrcode").width=$("qrcode").height=100;
-}
+
 html("ip",headers.ip);
 $("ip").href="http://ip.cn/index.php?ip="+headers.ip;
+
+var width=headers.url.length>100?headers.url.length:100;
+
+var qrcode = new QRCode("qrcode", {
+    text: headers.url,
+    width: width,
+    height: width,
+    colorDark : "#000000",
+    colorLight : "#ffffff",
+    correctLevel : QRCode.CorrectLevel.H
+});
